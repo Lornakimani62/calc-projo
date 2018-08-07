64c34d41101bf9398e10b374d86434c29cce1d66
lorna nashipae <kimanilorna@yahoo.com>
2018-08-07 09:50:40 +0300

    This commit adds a javaScript folder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add=function(number1, number2, number3, number4, number5){
+return number1+number2;
+};
+var number1=parseInt(prompt("Enter the first number"));
+var number2=parseInt(prompt("Enter the second number"));
+var result=add(number1, number2);
+alert("The result is "+result+"");
